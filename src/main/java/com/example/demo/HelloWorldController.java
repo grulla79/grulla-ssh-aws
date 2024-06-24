8f2ebf7744d76c5c2592eb1bc188318b165e7365
grulla79 <wnsgh9826@naver.com>
2024-06-24 12:03:42 +0900

    3

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 
 @RestController
 public class HelloWorldController {
